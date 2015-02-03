1dfd1840fe5bf225543ba3ea5317a5b4f696cb2d
Dainius <dainiusgri@gmail.com>
2015-02-02 23:55:22 -0800

    added stdlib functions

--- kernel/stdlib.c
+++ kernel/stdlib.c
@@ -4,13 +4,36 @@
 
 int k_strlen(const char* str)
 {
+	int i;
+	for(i=0; *str != '\0'; i++){
+		str++;
+	}
+	return i;
 }
 
 void* k_memcpy(void* dst, const void* src, int len)
 {
+	int i;
+	for(i=0; i<len; i++){
+		(char *)dst = (char *)src;
+		*dst++;
+		*src++;
+	}
+	return dst;
 }
 
 int k_memcmp(const void* b1, const void* b2, int len)
 {
+	int i;
+	for(i=0; i<len; i++){
+		if((unsigned char *)b1 == (unsigned char *)b2){
+			*b1++;
+			*b2++;
+		}
+		else {
+			return (unsigned char *)b1 - (unsigned char *)b2;
+		}
+	}
+	return 0;
 }
 
