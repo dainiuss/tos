c60374563688c8e729a04e59298f6acc8d431ce8
Dainius <dainiusgri@gmail.com>
2015-02-03 23:03:01 -0800

    fixed bug in stdlib

--- kernel/stdlib.c
+++ kernel/stdlib.c
@@ -13,25 +13,29 @@ int k_strlen(const char* str)
 
 void* k_memcpy(void* dst, const void* src, int len)
 {
+	char *xdst = (char *)dst;
+	char *xsrc = (char *)src;
 	int i;
 	for(i=0; i<len; i++){
-		(char *)dst = (char *)src;
-		*dst++;
-		*src++;
+		*xdst = *xsrc;
+		*xdst++;
+		*xsrc++;
 	}
 	return dst;
 }
 
 int k_memcmp(const void* b1, const void* b2, int len)
 {
+	unsigned char *xb1 = (unsigned char *)b1;
+	unsigned char *xb2 = (unsigned char *)b2;
 	int i;
 	for(i=0; i<len; i++){
-		if((unsigned char *)b1 == (unsigned char *)b2){
-			*b1++;
-			*b2++;
+		if(*xb1 == *xb2){
+			*xb1++;
+			*xb2++;
 		}
 		else {
-			return (unsigned char *)b1 - (unsigned char *)b2;
+			return *xb1 - *xb2;
 		}
 	}
 	return 0;
