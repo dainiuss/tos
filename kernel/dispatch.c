d3bead065d570c624f08e90e57cfb4a359968bdf
Dainius <dainiusgri@gmail.com>
2015-02-08 23:33:48 -0800

    added ad_redy_queue function

--- kernel/dispatch.c
+++ kernel/dispatch.c
@@ -9,21 +9,76 @@ PROCESS active_proc;
 
 /*
  * Ready queues for all eight priorities.
+ * 8 element-sized array ordered by process priority
+ * 0 - lowest priority
+ * 7 - highest priority
  */
 PCB *ready_queue [MAX_READY_QUEUES];
 
+/*
+ * The bits in ready_procs tell which ready queue is empty.
+ * The MSB (most significant bit) of ready_procs corresponds
+ * to ready_queue[7].
+ */
+unsigned ready_procs;
 
 
+/*
+
+typedef struct _PCB {
+	unsigned       magic;
+	unsigned	   used;
+	unsigned short priority;
+	unsigned short state;
+	MEM_ADDR	   esp;
+	PROCESS        param_proc;
+	void*		   param_data;
+	PORT           first_port;
+	PROCESS        next_blocked;
+	PROCESS        next;
+	PROCESS		   prev;
+	char*          name;
+} PCB;
+
+typedef PCB* PROCESS;
+
+ */
 
 /*
  * add_ready_queue
  *----------------------------------------------------------------------------
  * The process pointed to by p is put the ready queue.
- * The appropiate ready queue is determined by p->priority.
+ * The appropriate ready queue is determined by p->priority.
  */
 
 void add_ready_queue (PROCESS proc)
 {
+	int process_priority;
+	assert(proc->magic == MAGIC_PCB);
+	process_priority = proc->priority;
+	if(ready_queue[process_priority == NULL]){
+		/* The only process on this priority level */
+		ready_queue[process_priority] = proc;
+		proc->next = proc;
+		proc->prev = proc;
+		ready_procs |= 1 << process_priority;
+		// 00000001 << process_priority;
+		// 1 << 0  same as 1 * 2^0
+		// 1 << 1  same as 1 * 2^1
+		// 1 << 2  same as 1 * 2^2
+		// 1 << 3  same as 1 * 2^3
+		// ...
+		// 1 << 7  same as 1 * 2^7 = 128
+	}
+	else {
+		/* Other processes are present on this priority level*/
+		proc->next = ready_queue[process_priority];
+		proc->prev = ready_queue[process_priority]->prev;
+		ready_queue[process_priority]->prev->next = proc;
+		ready_queue[process_priority]->prev       = proc;
+	}
+
+	proc->state = STATE_READY;
 }
 
 
